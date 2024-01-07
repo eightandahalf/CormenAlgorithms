5a9a5f7b0edcb13c6d4084b58d6f6fa84c082e7d
eightandahalf <hleb.arbuzau@gmail.com>
2024-01-07 21:07:50 +0300

    commit

--- FindMaximumSubarray/FindMaximumSubarray/main.c
+++ FindMaximumSubarray/FindMaximumSubarray/main.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct max_subarr_val_struct {
-	int* array;
-	int low, mid, high;
-	int maxleft, maxright;
-	int leftsum_plus_rightsum;
-} typedef max_subarr_val_struct;
+struct lhs { // lhs - "low high sum" attributes
+	int low, high, sum;
+} typedef lhs;
 
-
-void find_max_crossing_subarray(max_subarr_val_struct* max_subarr_obj)
+lhs* find_max_crossing_subarray(int* array, int low, int mid, int high)
 {
+	lhs* lhs_obj = (lhs *)malloc(sizeof(lhs));
+
 	int leftsum = -2147483647, rightsum = -2147483647;
 	int sum = 0;
 	int MaxLeftIndex = 0, MaxRightIndex = 0;
 
-	for (int i = max_subarr_obj->mid; i >= max_subarr_obj->low; i--)
+	for (int i = mid; i >= low; i--)
 	{
-		sum += max_subarr_obj->array[i];
+		sum += array[i];
 		if (sum > leftsum)
 		{
 			leftsum = sum;
@@ -26,44 +24,85 @@ void find_max_crossing_subarray(max_subarr_val_struct* max_subarr_obj)
 		}
 	}
 	sum = 0;
-	for (int i = (max_subarr_obj->mid + 1); i <= max_subarr_obj->high; i++)
+	for (int i = (mid + 1); i <= high; i++)
 	{
-		sum += max_subarr_obj->array[i];
+		sum += array[i];
 		if (sum > rightsum)
 		{
 			rightsum = sum;
 			MaxRightIndex = i;
 		}
 	}
-	max_subarr_obj->maxleft = MaxLeftIndex;
-	max_subarr_obj->maxright = MaxRightIndex;
-	max_subarr_obj->leftsum_plus_rightsum = leftsum + rightsum;
+	lhs_obj->low = MaxLeftIndex;
+	lhs_obj->high = MaxRightIndex;
+	lhs_obj->sum = leftsum + rightsum;
+
+	return lhs_obj;
+}
+
+lhs* find_max_subarray(int* array, int low, int high)
+{
+	lhs* lhs_obj = (lhs *)malloc(sizeof(lhs));
+	lhs* left_lhs_obj = (lhs *)malloc(sizeof(lhs));
+	lhs* right_lhs_obj = (lhs *)malloc(sizeof(lhs));
+	lhs* cross_lhs_obj = (lhs *)malloc(sizeof(lhs));
+	int mid = 0;
+
+	if (high == low)
+	{
+		lhs_obj->low = low;
+		lhs_obj->high = high;
+		lhs_obj->sum = array[low];	
+		free(left_lhs_obj); free(right_lhs_obj); free(cross_lhs_obj);
+		return lhs_obj;
+	}
+	else {
+		mid = (high + low) / 2;
+		left_lhs_obj = find_max_subarray(array, low, mid);
+		right_lhs_obj = find_max_subarray(array, mid + 1, high);
+		cross_lhs_obj = find_max_crossing_subarray(array, low, mid, high);
+
+		if (left_lhs_obj->sum >= right_lhs_obj->sum && left_lhs_obj->sum >= cross_lhs_obj->sum)
+		{
+			free(lhs_obj); free(right_lhs_obj); free(cross_lhs_obj);
+			return left_lhs_obj;
+		}
+		else if (right_lhs_obj->sum >= left_lhs_obj->sum && right_lhs_obj->sum >= cross_lhs_obj->sum)
+		{
+			free(lhs_obj); free(left_lhs_obj); free(cross_lhs_obj);
+			return right_lhs_obj;
+		}
+		else
+		{
+			free(lhs_obj); free(left_lhs_obj); free(right_lhs_obj);
+			return cross_lhs_obj;
+		}
+	}
 }
 
 int main()
 {
 	int array_size = 10;
 	srand(time(NULL));   // Initialization, should only be called once.
-
-	max_subarr_val_struct* max_subarr_obj = malloc(sizeof(max_subarr_val_struct));
-	max_subarr_obj->low = 0;
-	max_subarr_obj->mid = (array_size / 2) - 1;
-	max_subarr_obj->high = array_size - 1;
-	max_subarr_obj->array = malloc(array_size * sizeof(int));
+	int* array = (int*)malloc(sizeof(int) * array_size);
+	int high, low;
+	high = array_size - 1;
+	low = 0;
 
 	for (int i = 0; i < array_size; i++)
 	{
-		max_subarr_obj->array[i] = (rand() % 200) - 100;
-		printf("%d ", max_subarr_obj->array[i]);
+		array[i] = (rand() % 200) - 100; // random negative and positive numbers
+		printf("%d ", array[i]);
 	}
 
-	find_max_crossing_subarray(max_subarr_obj);
+	lhs* lhs_obj = find_max_subarray(array, low, high);
 
-	printf("\n%d", max_subarr_obj->maxleft);
-	printf("\n%d", max_subarr_obj->maxright);
-	printf("\n%d\n", max_subarr_obj->leftsum_plus_rightsum);
+	printf("\nHigh index of array equal to %d",lhs_obj->high);
+	printf("\nLow index of array equal to %d", lhs_obj->low);
+	printf("\nSum of maxsum subarray equal to %d\n", lhs_obj->sum);
 
-	free(max_subarr_obj);
+	free(array);
+	free(lhs_obj);
 
 	printf("Press any key to exit...\n");
 	getchar();
